8acbbcdaee1b5262a095a1b8841a9cb8e8588389
davimury <davi.j.mury@gmail.com>
2022-03-22 17:41:29 -0300

    fix senhas com @

--- db_main.py
+++ db_main.py
@@ -1,12 +1,16 @@
 import pymysql
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
+import urllib.parse
+
 
 def iniciar_db(user, password, host, port, database):
     Base = automap_base()
     
+    password = urllib.parse.quote_plus(password)
+    
     # engine, suppose it has two tables 'user' and 'address' set up
-    engine = create_engine(f"mariadb+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8mb4")
+    engine = create_engine(f"mariadb+pymysql://%s:%s@%s:%s/%s?charset=utf8mb4" % (user, password, host, port, database))
 
     # reflect the tables
     Base.prepare(engine, reflect=True)
